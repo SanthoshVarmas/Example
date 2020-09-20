4fb54e505b22b9ae78835719a59f6a082320b0c9
SanthoshVarmaS <santhoshvarmas1111@gmail.com>
2020-09-19 22:03:48 -0500

    ooooo

--- src/hello.java
+++ src/hello.java
@@ -3,8 +3,9 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        System.out.println("master");
 
-
+        System.out.println("sunand- i am a branch");
 	}
 
 }
